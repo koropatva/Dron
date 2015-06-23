a306edb24a9a0a2ad724ca5b495d1fee01759a17
Andrew Koropatva <koropatvaua@gmail.com>
2015-06-23 18:22:52 +0300

    Changed url

--- dron-standalone/src/main/java/com/dron/sender/controllers/root/models/HistoryUiPlugin.java
+++ dron-standalone/src/main/java/com/dron/sender/controllers/root/models/HistoryUiPlugin.java
@@ -1,5 +1,13 @@
 package com.dron.sender.controllers.root.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.dron.sender.pattern.models.transformers.TransformKey;
+import com.dron.sender.pattern.services.transformers.TransformerFactory;
+import com.dron.sender.sequence.models.Param;
+import com.dron.sender.sequence.utils.ParamsUtils;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
@@ -25,7 +33,11 @@ public class HistoryUiPlugin {
 
 	@Override
 	public String toString() {
-		return uiPlugin.getUrl().get();
+		List<Param> params = new ArrayList<Param>();
+		TransformerFactory.reverseTransformEntity(params, uiParams,
+				TransformKey.ROOT_PARAMS);
+
+		return ParamsUtils.fillDataParams(uiPlugin.getUrl().get(), params);
 	}
 
 }
